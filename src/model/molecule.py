e3b7824409c3cbc633e0f4cb5d943c9f47435752
gatorrealbaespinola-debug <ga.torrealbaespinola@studenti.unitn.it>
2025-11-27 18:39:38 +0100

    mol

--- src/model/molecule.py
+++ src/model/molecule.py
@@ -1,12 +1,12 @@
 # Convert SELFIES to SMILES to RDKit molecules
 # Validity Checks
 # Caching fingerprints (for novelty calc)
-# MMFF otpimization wrapper (energy computation)
+# MMFF optimization wrapper (energy computation)
 
 # This is a trial version and can be changed later
 import selfies as sf
 from rdkit import Chem
-from rdkit.Chem import AllChem
+from rdkit.Chem import rdMolDescriptors, rdDistGeom, rdForceFieldHelpers, Crippen
 
 class Molecule:
     def __init__(self, selfies_str: str):
@@ -16,14 +16,37 @@ class Molecule:
         self.heavy_atom_count = self.rdkit_mol.GetNumHeavyAtoms()
         self.novelty = None
         self.mmff_energy = None
-        self.logp = None
-        self.tpsa = None
         self.fingerprint = None
+        self.energy = None
+        self.tpsa = rdMolDescriptors.CalcTPSA(self.rdkit_mol)
+        self.log_p = Crippen.MolLogP(self.rdkit_mol)
         self.fitness = None
         self.num_carbons = None
+
+    def compute_fingerprint(self):
+        if self.fingerprint is None:
+            self.fingerprint = rdMolDescriptors.GetMorganFingerprintAsBitVect(
+                self.rdkit_mol, radius=2, nBits=2048
+            )
+        return self.fingerprint
+
+    def compute_mmff_energy(self):
+        if self.energy is not None:
+            return self.energy
+        try:
+            mol = Chem.AddHs(self.rdkit_mol)
+            rdDistGeom.EmbedMolecule(mol)
+            result = rdForceFieldHelpers.MMFFOptimizeMolecule(mol)
+            props = rdForceFieldHelpers.MMFFGetMoleculeProperties(mol)
+            ff = rdForceFieldHelpers.MMFFGetMoleculeForceField(mol, props)
+            self.energy = ff.CalcEnergy()
+        except Exception:
+            self.energy = float("inf")
+        return self.energy
 
     def count_carbons(self):
         """Return the number of carbon atoms in an RDKit mol."""
         if self.rdkit_mol is None:
             return 0
         self.num_carbons = sum(1 for atom in self.rdkit_mol.GetAtoms() if atom.GetAtomicNum() == 6)
+        return self.num_carbons # TODO Verify
